cdbc20bc107fde957983144ec863f3c974ffbd16
hackingyseguridad <antonio_taboada@hotmail.es>
2022-03-30 12:45:02 +0200

    Update localspy3.sh

--- localspy3.sh
+++ localspy3.sh
@@ -5,4 +5,4 @@ echo
 # tcpdump host IP  ( captura todo lo entra y sale a una IP )
 # tcpdump -s0 port 383  ( filta por puerto )
 # tcpdump -s0 not port 22 and port 636 ( filtra por puerto y evita captura de otro )
-tcpdump -i eth0 -s0 -w -vvv - not port 22 and port 636 -w /tmp/captura.pcap |stdbuf -oL tcpick -C -v -a -h -yP -r -
+tcpdump -i eth0 -s0 -w -vvv not port 22 and port 636 -w /tmp/captura.pcap |stdbuf -oL tcpick -C -v -a -h -yP -r -
