ad57a86b74ddff0cb6b98787928d9ef78d40d212
hackingyseguridad <antonio_taboada@hotmail.es>
2022-03-30 13:39:58 +0200

    Update localspy3.sh

--- localspy3.sh
+++ localspy3.sh
@@ -13,4 +13,5 @@ echo
 # tcpdump -i eth0 -A ( Muestra los datos en Ascii )
 # tcpdump -i eth0 -XX ( Muestra los datos en formato Hexadecimal y ascii )
 # tcpdump -i eth0 -s0 -vvv not port 22 and port 636 -w /tmp/captura.pcap |stdbuf -oL tcpick -C -v -a -h -yP -r 
+# tcpdump port http or port ftp or port smtp or port imap or port pop3 or port telnet -l -A | egrep -i -B5 'pass=|pwd=|log=|login=|user=|username=|pw=|passw=|passwd=|password=|pass:|user:|username:|password:|login:|pass |user '
 tcpdump -i eth0 -s0 -vvv not port 22
